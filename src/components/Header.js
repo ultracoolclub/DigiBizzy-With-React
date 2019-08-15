0bbdea4ff55c4a7eb10d776bc32308ea49d86fa7
ultracoolclub <jeremiah.ashcraft@gmail.com>
2019-08-15 00:18:34 -0400

    adds header with navbar and some css style

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,17 @@ import React from "react"
 function Header() {
     return(
         <div>
-            
+            <header className="header">
+                <h1 className="logo">Stock Info</h1>
+                <nav className="navBar">
+                    <ul className="navList">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Reading</li>
+                    </ul>
+                </nav>
+            </header>
         </div>
     )
 }
